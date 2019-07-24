fa57aef575af0f67c277c83910a2bef27fa19db0
LiJuncongDIKU <ljc1350220@163.com>
2019-07-24 09:26:33 +0800

    ‘新版double ball

--- vant实验项目/vantTest/src/components/doubleColorBall.vue
+++ vant实验项目/vantTest/src/components/doubleColorBall.vue
@@ -1,25 +1,41 @@
 <template>
     <div class="center">
+        <Alert v-if="waiting">
+            <Icon type="md-sync" />正在处理……
+        </Alert>
         <Row>
             <Col span="8">
                 <table>
                     <tr>
-                        <td v-for="(item, index) in redArr" :key="index">
+                        <td
+                            v-if="redArr.length"
+                            v-for="(item, index) in redArr"
+                            :key="'oneTime'+index"
+                        >
                             <div class="red ball">{{item}}</div>
                         </td>
                         <td>
                             <div class="blue ball">{{blueA}}</div>
                         </td>
                         <td>
-                            <Button type="warning" size="large" @click="changeBalls">Go!</Button>
+                            <div>
+                                一次统计次
+                                <Input-number :min="1" v-model="clTimes" inline></Input-number>
+                                <Button
+                                    type="warning"
+                                    size="large"
+                                    @click="changeBallsTimes"
+                                    :disabled="disableGo"
+                                >Go!</Button>
+                            </div>
                         </td>
                     </tr>
                 </table>
             </Col>
-            <Col span="8">
-                <table>
-                    <tr v-for="(group, index) in history" :key="index">
-                        <td v-for="(redBall, idx) in group[0]" :key="idx">
+            <!-- <Col span="8">
+                <table style="max-height:500px; overflow:auto;display: block;">
+                    <tr v-for="(group, index) in history" :key="'group'+index">
+                        <td v-for="(redBall, idx) in group[0]" :key="'redGp'+idx">
                             <div class="red ball">{{redBall}}</div>
                         </td>
                         <td>
@@ -27,11 +43,22 @@
                         </td>
                     </tr>
                 </table>
-            </Col>
+            </Col> -->
             <Col span="8">
                 <!-- 统计处 -->
                 <!-- 出现次表 -->
                 <div id="timesChart" class="chart-box"></div>
+                <h2>{{history.length}}次统计建议:</h2>
+                <div
+                    v-for="(item, index) in hopeRedBalls"
+                    :key="'resRed'+index"
+                    class="ball red"
+                >{{item.num}}</div>
+                <div
+                    v-for="(item, index) in blueBalls"
+                    :key="'resBlue'+index"
+                    class="ball blue"
+                >{{item}}</div>
             </Col>
         </Row>
     </div>
@@ -45,15 +72,43 @@ export default {
             redArr: [],
             blueA: 0,
             history: [],
-            timesChart: null
+            timesChart: null,
+            hopeRedBalls: [],
+            blueBalls: [],
+            disableGo: false,
+            clTimes: 1,
+            waiting: false
         };
     },
     mounted() {
         const that = this;
-        that.timesChart = that.$echarts.init(document.getElementById("timesChart"));
-        that.changeBalls();
+        that.timesChart = that.$echarts.init(
+            document.getElementById("timesChart")
+        );
+        // that.changeBalls();
     },
     methods: {
+        // 批量跑
+        changeBallsTimes() {
+            const that = this;
+            // let itl = setInterval(function() {
+            //     if (!that.clTimes) {
+            //         clearInterval(itl);
+            //     } else {
+
+            //     }
+            // }, 300);
+            that.disableGo = true;
+            that.waiting = true;
+            while (that.clTimes) {
+                that.changeBalls();
+                that.clTimes--;
+            }
+            that.waiting = false;
+            that.disableGo = false;
+            that.clTimes = 1;
+            that.updateTimesChart();
+        },
         changeBalls() {
             const that = this;
             let rd = 0;
@@ -71,18 +126,19 @@ export default {
 
             //写入历史
             that.history.unshift([that.redArr, that.blueA]);
-            that.updateTimesChart();
         },
         getRandomLimit(limit) {
             let rd = Math.random() * 100 + limit;
             rd %= limit;
-            return Number((++rd).toFixed(0));
+            rd++;
+            return rd > limit ? limit : rd;
         },
         sortRed(a, b) {
             return a - b;
         },
         updateTimesChart() {
             const that = this;
+            that.disableGo = true;
             let opt = {
                 tooltip: {
                     trigger: "axis",
@@ -122,13 +178,49 @@ export default {
                         name: "红球",
                         type: "bar",
                         data: (function(params) {
-                            let res = new Array(33).fill(0);
+                            let res = new Array(33)
+                                .fill({})
+                                .map((currentValue, index) => {
+                                    return { num: index + 1, times: 0 };
+                                });
                             that.history.forEach(group => {
                                 group[0].forEach(ele => {
-                                    res[ele - 1]++;
+                                    // console.log(ele - 1);
+
+                                    res[ele - 1].times++;
                                 });
                             });
-                            return res;
+                            // console.log(res);
+
+                            that.hopeRedBalls = [];
+                            let temp = new Array(33)
+                                .fill({})
+                                .map((currentValue, index) => {
+                                    return res[index];
+                                });
+                            // console.log("temp:",temp);
+                            temp.sort((a, b) => {
+                                return b.times - a.times;
+                            });
+                            let lastTime = 0; // 最后一个球出现的次数
+                            while (true) {
+                                let hopeLength = that.hopeRedBalls.length;
+                                let aBall = temp[hopeLength];
+                                if (
+                                    hopeLength >= 6 &&
+                                    aBall.times != lastTime
+                                ) {
+                                    break;
+                                }
+                                that.hopeRedBalls.push(aBall);
+                                lastTime = aBall.times;
+                            }
+                            that.hopeRedBalls.sort(function(a, b) {
+                                return a.num - b.num;
+                            });
+                            return res.map(currentValue => {
+                                return currentValue.times;
+                            });
                         })()
                     },
                     {
@@ -139,12 +231,35 @@ export default {
                             that.history.forEach(group => {
                                 res[group[1] - 1]++;
                             });
+                            // console.log(res);
+                            let max = 0;
+                            that.blueBalls = [];
+                            res.forEach((ele, index) => {
+                                // console.log(index,ele);
+                                if (ele > max) {
+                                    that.blueBalls = [];
+                                    max = ele;
+                                    that.blueBalls.push(index + 1);
+                                } else if (ele == max) {
+                                    let flag = true;
+                                    that.blueBalls.forEach(ball => {
+                                        if (ball == index + 1) {
+                                            flag = false;
+                                        }
+                                    });
+                                    if (flag) {
+                                        that.blueBalls.push(index + +1);
+                                    }
+                                }
+                            });
                             return res;
                         })()
                     }
                 ]
             };
+            // console.log(that.history)
             that.timesChart.setOption(opt);
+            that.disableGo = false;
         }
     }
 };
